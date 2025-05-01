9b095e1a728df84c2fb4ae9e7e30c081c703ec1b
LUCHER4321 <lucher4321@gmail.com>
2025-05-01 19:39:31 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 import { deployServer } from "./servers/server-with-mysql.js";
+const PORT = process.env.PORT ?? 3000;
 
-deployServer();
+const app = deployServer();
+app.listen(PORT, () => {
+    console.log(`Server listening on port http://localhost:${PORT}`);
+});
