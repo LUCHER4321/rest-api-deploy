fac160395433cc3b1232ed08394830c4e73eea88
LUCHER4321 <lucher4321@gmail.com>
2025-05-01 19:39:28 -0400

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ import { corsMiddleware } from './middlewares/cors.js';
 
 export const createApp = ({ movieModel }) => {
     const app = express();
-    const PORT = process.env.PORT ?? 3000;
     
     app.disable("x-powered-by");
     
@@ -17,8 +16,5 @@ export const createApp = ({ movieModel }) => {
     });
     
     app.use("/movies", createMovieRouter({ movieModel }));
-    
-    app.listen(PORT, () => {
-        console.log(`Server listening on port http://localhost:${PORT}`);
-    });
+    return app;
 };
